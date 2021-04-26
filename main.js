21c78c642518a42ecc35eed2e7f459a37ebf2eef
MartaKirchgessner <martakirchgessner@gmail.com>
2021-04-26 09:42:09 +0200

    set up DaysInfo

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 const header = document.querySelector('header.header-news');
+const mainContent = document.querySelector('section.main-content');
 const carouselItemCount = 4;
+const daysInfoCount = 32;
+
+function populateDaysInfo() {
+  for (let i = 1; i < daysInfoCount; i++) {
+    const day = document.createElement('div');
+    day.classList.add('main-content_day');
+    day.innerHTML = [i];
+    mainContent.appendChild(day);
+  }
+}
+populateDaysInfo();
 
 function createDivForNews(newsContents) {
   const newsArticle = document.createElement('div');
   newsArticle.innerText = newsContents.title; //vytahneme titul z objektu
   return newsArticle;
 }
-
-fetch('http://localhost:3000/news.json')
-  .then((serverResponse) => serverResponse.text())
-  .then((responseText) => {
-    const data = JSON.parse(responseText);
-    populateNewsCarousel(data.articles);
-  });
 
 function populateNewsCarousel(news) {
   for (let i = 0; i < carouselItemCount; i++) {
@@ -21,3 +26,10 @@ function populateNewsCarousel(news) {
     header.appendChild(newsDiv);
   }
 }
+
+fetch('http://localhost:3000/news.json')
+  .then((serverResponse) => serverResponse.text())
+  .then((responseText) => {
+    const data = JSON.parse(responseText);
+    populateNewsCarousel(data.articles);
+  });
